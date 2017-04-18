27cdc965f942d2e8a85cd995e9557a93679468be
Maxime Catrice <max.catrice@gmail.com>
2017-04-18 18:24:45 +0200

    Final commit

--- compile.bat
+++ compile.bat
@@ -1,12 +1,11 @@
 @echo off
-if exist src\Ants.class (
-	del src\Ants.class
+if exist bin\Ants.class (
+	del bin\Ants.class
 )
-if exist src\Aco.class (
-	del src\Aco.class
+if exist bin\Aco.class (
+	del bin\Aco.class
 )
-if exist src\City.class (
-	del src\City.class
+if exist bin\City.class (
+	del bin\City.class
 )
 javac -d bin\ src\City.java src\Ants.java src\Aco.java
-pause
